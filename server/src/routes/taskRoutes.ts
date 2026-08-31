f8e47feee9c9e68f50c5dfa9ff2f050b5ed8cad7
tanuLivi <tanurialahapperuma@gmail.com>
2026-08-31 09:17:12 +0530

    Revert "connected backend with task status page and view project page"
    This reverts commit 98b5908b7288562c05691223c309d85d23105b0e.

--- server/src/routes/taskRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getProjectTasks } from "../controllers/taskController";
-
-const router = Router();
-
-// Route: GET /api/tasks/:projectId
-router.get("/:projectId", getProjectTasks);
-
-export default router;
